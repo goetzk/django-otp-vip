ebce1dfb684227bac9243a6ba9ec63d5b583a33d
Karl Goetz <karl@kgoetz.id.au>
2017-12-06 13:24:58 +1100

    Expand admin pages
    UserAdmin is now available via admin pages to facilitate easily searching and
    viewing users (its a read only page, users can't be added or modified).

--- admin.py
+++ admin.py
@@ -5,15 +5,26 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import VipPushDevice, VipTokenDevice
+from .device_models import VipPushDevice, VipTokenDevice
+from models import VipUser
 
+
+@admin.register(VipUser)
+class UserAdmin(admin.ModelAdmin):
+  """Provides administrative functions for the model."""
+  readonly_fields = ['vip_user_id', 'vip_created_at', 'status', 'bindings_count', 'pin_set', 'pin_expiration_time', 'temp_password_set']
+  list_filter = ['vip_created_at', 'status', 'temp_password_set', 'pin_expiration_time']
+  list_display = ['user', 'vip_user_id', 'status', 'vip_created_at', 'bindings_count']
+  search_fields = ['vip_user_id', 'user__username', '']
 
 @admin.register(VipPushDevice)
 class PushDeviceAdmin(admin.ModelAdmin):
   """Provides administrative functions for the model."""
-  readonly_fields = ['last_updated', 'latest_transaction_id']
-  list_filter = ['last_updated', 'confirmed']
-  search_fields = ['latest_transaction_id', 'user__username', 'confirmed']
+  # FIXME: all fields read only
+  readonly_fields = ['credential_id']
+  list_filter = ['credential_type', 'credential_status', 'token_form_factor', 'token_kind', 'last_authn_time']
+  search_fields = ['credential_id', 'user__username', 'token_kind', 'token_adaptor', 'push_enabled', 'friendly_name']
+  list_display = ['user', 'credential_id', 'credential_type', 'friendly_name', 'last_authn_time']
 
 @admin.register(VipTokenDevice)
 class TokenDeviceAdmin(admin.ModelAdmin):
