ace542f1bb158c467a919f706b306ed1f8f5c883
Karl Goetz <karl@kgoetz.id.au>
2018-01-05 10:33:10 +1100

    Refactor run_otp
    Fix a bug with missing utils. prefix, resulting in silent failure.
    Fix an ordering bug where user details are re checked on every page load
    instead of only on initial page load.

--- views.py
+++ views.py
@@ -50,28 +50,6 @@ def run_otp(request, display_template='django_otp_vip/validate_vip.html'):
   """
   logger.debug('In run_otp view')
 
-
-  # Update known details about user including credentials
-  try:
-    logger.debug('Attempting to update user details')
-    full_user_details = query_user_info(request.user.email)
-    if not full_user_details:
-      print('user does not exist, logging of error will occur later')
-      return False
-    updated_records = update_vip_user_records(full_user_details)
-  except Exception as ee:
-    # handle this better....
-    updated_records = None
-    print 'balalhasdlhfaldfh'
-    print ee
-
-  if updated_records:
-    print "The records were updated"
-    # check the return code is success
-    # that we have a user
-    # that they have >= 1 credential to validate with
-    pass
-
   # if this is a POST request we need to process the form data
   if request.method == 'POST':
       logger.debug('post method')
@@ -87,12 +65,12 @@ def run_otp(request, display_template='django_otp_vip/validate_vip.html'):
       logger.debug('are either forms valid?')
 
       # check if token is valid
-#       if token_form.is_valid():
-#         logger.debug('second factor token worked')
-#         return HttpResponse('/')
-#       else:
-#         logger.debug('token form errors')
-#         logger.debug(token_form.errors.as_data())
+      if token_form.is_valid():
+        logger.debug('second factor token worked')
+        return HttpResponse('/')
+      else:
+        logger.debug('token form errors')
+        logger.debug(token_form.errors.as_data())
 
       # check if the push is valid
       if push_form.is_valid():
@@ -104,13 +82,21 @@ def run_otp(request, display_template='django_otp_vip/validate_vip.html'):
 
   # if a GET (or any other method) we'll create a blank form
   else:
-      logger.debug('creating some empty forms')
-      # if some push credentials are available, show form. Otherwise don't show.
-      push_form = PushForm(request.user)
-      # ditto token credentials
-      token_form  = TokenForm(request.user)
+
+    logger.debug('Attempting to update user details')
+    full_user_details = utils.query_user_info(request.user.email)
+
+    if update_vip_user_records(full_user_details):
+      logger.debug("Records for {0} were updated".format(request.user))
+    else:
+      logger.debug('Unable to update records for {0}'.format(request.user))
+
+    logger.debug('creating some empty forms')
+    # if some push credentials are available, show form. Otherwise don't show.
+    push_form = PushForm(request.user)
+    # ditto token credentials
+    token_form  = TokenForm(request.user)
 
   # TODO: check if there are devices that will work with the two types of forms and if not set None
- #  return render(request, display_template, {'formpush': push_form, 'formtoken': token_form })
-  return render(request, display_template, {'formpush': push_form })
+  return render(request, display_template, {'formpush': push_form, 'formtoken': token_form })
 
