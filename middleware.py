787c89c37821b48151bc77c7e37ba2ebba09f401
Karl Goetz <karl@kgoetz.id.au>
2018-03-08 16:14:00 +1100

    Enforce user logins via OTP in middleware
    Django-OTP doesn't support an optional second factor login (like
    if_configured=True) for its authentication forms.
    
    This middleware is provided for when you want people with OTP configured to
    always use it, and those without it configured to never use it.
    
    This effectively reverts and replaces the previous commit "Add VIP component of
    global OTP logins".

---
+++ middleware.py
@@ -0,0 +1,49 @@
+# Set up logging first thing
+import logging
+logger = logging.getLogger(__name__)
+
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+
+from django_otp import user_has_device, _user_is_anonymous, _user_is_authenticated
+
+# https://stackoverflow.com/questions/2916966/non-global-middleware-in-django
+# was the best help I found here, with several good posts.
+
+otp_login_url = reverse('run_multi_factor') # This should probably be settings.OTP_LOGIN
+excluded_urls_list = ['/accounts/logout/', otp_login_url]
+
+class MandatoryOtpMiddleware(object):
+  """Ensure all users configured for OTP are using it.
+
+  This middleware uses data set by django_otp.middleware.OTPMiddleware so must
+  be installed after it in Django middleware settings.
+  """
+
+  def process_request(self, request):
+    """
+    Automatically ensure users who should be logged in via OTP are.
+    """
+    current_url = request.path_info
+
+    # We don't need to worry about annon users
+    if _user_is_anonymous(request.user):
+      return None
+
+    # A select few views can't be captured. For example
+    # * Logout means we can never leave the site
+    # * OTP login means a redirect loop when viewing the otp login url.
+    if current_url in excluded_urls_list:
+      logger.debug('Currently requested url ({0}) is in excluded urls list ({1})'.format(current_url, excluded_urls_list))
+      return None
+
+    # Only need to check this if they are logged in
+    if _user_is_authenticated(request.user):
+      # If they have at least one device they should be logged in via OTP
+      if user_has_device(request.user):
+        if not request.user.is_verified():
+          logger.debug('{0} is authenticated but not verified. Redirecting to {1}'.format(request.user, otp_login_url))
+          return HttpResponseRedirect(otp_login_url)
+        # else, they are verified and are OK
+      # else they have no device; they don't need to have OTP
+
