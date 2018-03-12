ce12a80ee6cb318a7fe4841f355c2d9ffeff17a7
Karl Goetz <karl@kgoetz.id.au>
2018-03-12 13:40:07 +1100

    Add url to manage VIP settings
    I think this is worth having enabled by default as it allows us to link to it
    in other templates and be sure it will exist.

--- urls.py
+++ urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import url
 
 from django.core.urlresolvers import reverse_lazy
 
-from otp_vip.views import multi_factor, check_verification
+from otp_vip.views import multi_factor, check_verification, manage_two_factor
 
 urlpatterns = [
     url(r'^login/?$', multi_factor, name='run_multi_factor'),
     url(r'^test_multi$', check_verification, name='test_multi'),
-    # url(r'^managevip$', manage_two_factor, { 'template': 'myvip.html' }, name='manage_vip'),
+    url(r'^managevip$', manage_two_factor, { 'template': 'myvip.html' }, name='manage_vip'),
     ]
 
