8bb0738a557bcde64295668849d28ce276c3ce85
Karl Goetz <karl@kgoetz.id.au>
2018-03-12 20:12:42 +1100

    Add OTP management page
    This is a simple OTP management page ; replace with something nicer as your
    needs dictate.

--- urls.py
+++ urls.py
@@ -8,6 +8,6 @@ from otp_vip.views import multi_factor, check_verification, manage_two_factor
 urlpatterns = [
     url(r'^login/?$', multi_factor, name='run_multi_factor'),
     url(r'^test_multi$', check_verification, name='test_multi'),
-    url(r'^managevip$', manage_two_factor, { 'template': 'myvip.html' }, name='manage_vip'),
+    url(r'^managevip$', manage_two_factor, { 'template': 'otp_vip/myvip.html' }, name='manage_vip'),
     ]
 
