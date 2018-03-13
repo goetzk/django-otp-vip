7ea91ebf81cb409e967f49e471b56b20409227db
Karl Goetz <karl@kgoetz.id.au>
2018-03-13 19:40:40 +1100

    Refactor middleware class to improve inheritance
    I realised that it was tricky to add a new check (or replace the existing
    checks) with the middleware as it was written so I have  reworked it to try and
    address that issue.
    
    * Variables now set in the class and can be accessed/modified from there
    * Permissions check is separate from request processing

--- middleware.py
+++ middleware.py
@@ -10,15 +10,36 @@ from django_otp import user_has_device, _user_is_anonymous, _user_is_authenticat
 # https://stackoverflow.com/questions/2916966/non-global-middleware-in-django
 # was the best help I found here, with several good posts.
 
-otp_login_url = reverse('run_multi_factor') # This should probably be settings.OTP_LOGIN
-excluded_urls_list = ['/accounts/logout/', otp_login_url]
-
 class MandatoryOtpMiddleware(object):
   """Ensure all users configured for OTP are using it.
 
   This middleware uses data set by django_otp.middleware.OTPMiddleware so must
   be installed after it in Django middleware settings.
   """
+
+  otp_login_url = reverse('run_multi_factor') # This should probably be settings.OTP_LOGIN
+  excluded_urls_list = ['/accounts/logout/', otp_login_url]
+
+  def user_must_authenticate(self,request):
+    """Check to run against user on each request.
+
+    Note that this will slow down EVERY request it is called on, so try to keep
+    it snappy!
+    If you need to override the behaviour of this function you should consider
+    subclassing MandatoryOtpMiddleware and running this via Super() to preserve
+    the existing behaviour.
+
+    Return True if user must authenticate, False if they are fine.
+    """
+
+    # If they have at least one device they should be logged in via OTP
+    if user_has_device(request.user):
+      if not request.user.is_verified():
+        logger.debug('{0} is authenticated but not verified.'.format(request.user))
+        return True
+      # else, they are verified and are OK
+    # else they have no device; they don't need to have OTP
+    return False
 
   def process_request(self, request):
     """
@@ -33,17 +54,18 @@ class MandatoryOtpMiddleware(object):
     # A select few views can't be captured. For example
     # * Logout means we can never leave the site
     # * OTP login means a redirect loop when viewing the otp login url.
-    if current_url in excluded_urls_list:
-      logger.debug('Currently requested url ({0}) is in excluded urls list ({1})'.format(current_url, excluded_urls_list))
+    if current_url in self.excluded_urls_list:
+      logger.debug('Currently requested url ({0}) is in excluded urls list ({1})'.format(current_url, self.excluded_urls_list))
       return None
 
-    # Only need to check this if they are logged in
+    # Only need to check in more detail if they are logged in
     if _user_is_authenticated(request.user):
-      # If they have at least one device they should be logged in via OTP
-      if user_has_device(request.user):
-        if not request.user.is_verified():
-          logger.debug('{0} is authenticated but not verified. Redirecting to {1}'.format(request.user, otp_login_url))
-          return HttpResponseRedirect(otp_login_url)
-        # else, they are verified and are OK
-      # else they have no device; they don't need to have OTP
+      # Run our checks and redirect code, it lives apart to be easily overridden.
+      if self.user_must_authenticate(request):
+        # Returned true, send the user away
+        logger.debug('{0} needs to supply their second factor. Redirecting to {1}'.format(request.user, self.otp_login_url))
+        return HttpResponseRedirect(self.otp_login_url)
+
+    # If nothing else causes a redirect, return None to finish off
+    return None
 
