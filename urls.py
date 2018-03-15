8295867e76ef2eead8b3fe9dc9c770d56f7ddef0
Karl Goetz <karl@kgoetz.id.au>
2018-03-15 11:26:45 +1100

    Remove dead code
    Removing decorators.py (again) - Accidentally reintroduced in the commit it was
    supposed to be removed.
    
    Remove check_verification which was left over from commit "Add VIP component of
    global OTP logins".

--- urls.py
+++ urls.py
@@ -3,11 +3,10 @@ from django.conf.urls import url
 
 from django.core.urlresolvers import reverse_lazy
 
-from otp_vip.views import multi_factor, check_verification, manage_two_factor
+from otp_vip.views import multi_factor, manage_two_factor
 
 urlpatterns = [
     url(r'^login/?$', multi_factor, name='run_multi_factor'),
-    url(r'^test_multi$', check_verification, name='test_multi'),
     url(r'^managevip$', manage_two_factor, { 'template': 'otp_vip/myvip.html' }, name='manage_vip'),
     ]
 
